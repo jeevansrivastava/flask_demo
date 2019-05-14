918b6887381707e257a5ca3a9beed26a358e2421
Jeevan Srivastava <jeevan.srivastavajj@gmail.com>
2019-05-14 15:12:07 +0530

    Removal of unused imports

--- utility/Response.py
+++ utility/Response.py
@@ -1,13 +1,15 @@
 class Response:
-     # 2xx
+    # 2xx
     @staticmethod
-    def success(data,statuscode):
-         return {'status': 'success', 'data': data}, statuscode
+    def success(data, statuscode):
+        return {'status': 'success', 'data': data}, statuscode
+
     # 3xx
     @staticmethod
-    def redirect(data,statuscode):
-         return {'status': 'redirect', 'data': data}, statuscode
-     #4xx
+    def redirect(data, statuscode):
+        return {'status': 'redirect', 'data': data}, statuscode
+
+    # 4xx
     @staticmethod
-    def error(data,statuscode):
-         return {'status': 'error', 'data': data}, statuscode
+    def error(data, statuscode):
+        return {'status': 'error', 'data': data}, statuscode

1e3fc851d8c3b854b3cce6472700dcd55ab5561a
Jeevan Srivastava <jeevan.srivastavajj@gmail.com>
2019-05-29 12:20:40 +0530

    Endpoint sepration

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from resources.Hello import Hello
+from module.lead.resource import LeadResource
 from resources.Category import CategoryResource
 from resources.Comment import CommentResource
-
-from module.lead.resource import LeadResource  
-import module.user as user
-
-
-
+from resources.Hello import Hello
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
-
-
 
 # Route
 
@@ -22,12 +15,3 @@ api.add_resource(Hello, '/hello')
 api.add_resource(CategoryResource, '/Category')
 api.add_resource(CommentResource, '/Comment')
 api.add_resource(LeadResource, '/lead')
-api.add_resource(user.UserRegistration, '/registration')
-api.add_resource(user.UserLogin, '/login')
-api.add_resource(user.UserLogoutAccess, '/logout/access')
-api.add_resource(user.UserLogoutRefresh, '/logout/refresh')
-api.add_resource(user.TokenRefresh, '/token/refresh')
-api.add_resource(user.AllUsers, '/users')
-api.add_resource(user.SecretResource, '/secret')
-
-
---
+++ module/user/endpoint.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+from flask_restful import Api
+
+import module.user as user
+
+user_bp = Blueprint('user', __name__)
+api = Api(user_bp)
+
+# Route
+
+
+api.add_resource(user.UserRegistration, '/registration')
+api.add_resource(user.UserLogin, '/login')
+api.add_resource(user.UserLogoutAccess, '/logout/access')
+api.add_resource(user.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(user.TokenRefresh, '/token/refresh')
+api.add_resource(user.AllUsers, '/users')
+api.add_resource(user.SecretResource, '/secret')

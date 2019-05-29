a09563ff3dcb41d5599e9f50c066ea5143d8321b
Jeevan Srivastava <jeevan.srivastavajj@gmail.com>
2019-05-29 12:20:52 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 from flask import Flask
-from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from module.user.model import UserModel, RevokedTokenModel
+
+from module.user.model import RevokedTokenModel
 
 
 def create_app(config_filename):
     app = Flask(__name__)
     app.config.from_object(config_filename)
-    
+
     from app import api_bp
+    from module.user.endpoint import user_bp
+    app.register_blueprint(user_bp, url_prefix='/user')
     app.register_blueprint(api_bp, url_prefix='/api')
 
     from models import db
@@ -21,6 +23,8 @@ if __name__ == "__main__":
     app = create_app("config")
 
     jwt = JWTManager(app)
+
+
     @jwt.token_in_blacklist_loader
     def check_if_token_in_blacklist(decrypted_token):
         jti = decrypted_token['jti']
@@ -28,5 +32,3 @@ if __name__ == "__main__":
 
 
     app.run(debug=True)
-
-    
